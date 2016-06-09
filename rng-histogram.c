31baac735186ed84598f39402d10c6aa1bc3fea2
ugurcanpolat <ugurcan.polat12@gmail.com>
2016-06-10 02:40:34 +0300

    Styling fix and renaming the file

--- rng-histogram.c
+++ rng-histogram.c
@@ -1,8 +1,6 @@
-     /* ***************** */
-    /*     HOMEWORK-2    */
-   /*  	Ugurcan Polat   */
-  /*	  150140117    */
- /*	polatu@itu.edu.tr */
+   /* ***************** */
+  /*   Ugurcan Polat   */
+ /* polatu@itu.edu.tr */
 /* ***************** */
 
 #include <stdio.h> //printf(), scanf()
